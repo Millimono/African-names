8f75f18cebf94c427ae3d4362ff635b4c853c956
Millimono <millimono64.sm@gmail.com>
2025-03-10 13:22:24 +0000

    nav bar style mobile ajoutee

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <h2 className="logo">Prénoms Africains</h2>
-      <ul className="nav-links">
+
+      {/* Bouton Menu Burger en Mobile */}
+      <button className="burger-menu" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </button>
+
+      {/* Menu Responsive */}
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
         <li>
           <button onClick={() => console.log("Accueil")}>Accueil</button>
         </li>
